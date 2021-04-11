84b8be07bf09c1450da671f9135631695f122369
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-04-10 20:09:06 -0400

    initial commit

---
+++ footsteps/footsteps.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import subprocess
+
+print("name of run")
+run_name = input()
+output_dir = "results/" + run_name + "/"
+os.mkdir(output_dir)
+
+with open(output_dir + "info.txt", "w") as f:
+    f.write("Command:\n")
+    f.write(" ".join(sys.argv) + "\n")
+    f.write("System:\n")
+    f.write(subprocess.check_output(["hostname"]).decode())
+    f.write("Python:\n")
+    f.write(subprocess.check_output(["which", "python"]).decode())
+    f.write("Git Hash:\n")
+    f.write(
+        subprocess.check_output(["git", "describe", "--always"]).strip().decode() + "\n"
+    )
+    f.write("Uncommitted changes:\n")
+    f.write(
+        subprocess.check_output(
+            ["git", "diff", "HEAD", "--", ".", ":(exclude)*.ipynb"]
+        ).decode()
+    )
