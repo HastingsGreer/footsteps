baf3a112f567cc043fdc7bf596275653f6bb2038
HastingsGreer <hastings.greer@gmail.com>
2021-10-17 10:31:25 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ import os.path
 
 class FootstepsTestCase(unittest.TestCase):
     def testFootsteps(self):
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate("my_results_name")
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"my_results_name")
         
         self.assertTrue(os.path.exists("results/my_results_name/info.txt"))
         
