0da30c62c403f9b68fefb874d621cda1a71c33cf
HastingsGreer <hastings.greer@gmail.com>
2021-10-17 10:38:35 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -12,4 +12,11 @@ class FootstepsTestCase(unittest.TestCase):
         
         self.assertTrue(os.path.exists("results/my_results_name/info.txt"))
         
+    def testFootstepsFallbackPath(self):
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        
+        self.assertTrue(os.path.exists("results/preexisting_results_name(2)/info.txt"))
+        
         
