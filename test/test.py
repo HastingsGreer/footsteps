31c7eefbe2223e339f483be850d34db4b9b6dfdc
Hastings Greer <hastings@hgreer.com>
2022-06-14 14:00:10 -0400

    update test file

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import unittest
+import shutil
 import subprocess
 import time
 
@@ -6,43 +7,93 @@ from subprocess import PIPE, Popen
 
 import os.path
 
+
 class FootstepsTestCase(unittest.TestCase):
     def testFootsteps(self):
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"my_results_name")
-        
+        shutil.rmtree("results/my_results_name/", ignore_errors=True)
+        output = subprocess.Popen(
+            ["python", "test/example_program.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"my_results_name")
+
         self.assertTrue(os.path.exists("results/my_results_name/info.txt"))
-        
+
         with open("results/my_results_name/info.txt", "r") as f:
             print("Current info format:\n=========================")
             print(f.read())
-    
+
     def testCustomRoot(self):
-        output = subprocess.Popen(["python", "test/example_custom_root.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"my_results_name")
-        
+        shutil.rmtree("../results/my_results_name/", ignore_errors=True)
+        output = subprocess.Popen(
+            ["python", "test/example_custom_root.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"my_results_name")
+
         self.assertTrue(os.path.exists("../results/my_results_name/info.txt"))
         self.assertTrue(os.path.exists("../results/my_results_name/card.txt"))
-        
+
     def testFootstepsFallbackPath(self):
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
-        
+        shutil.rmtree("results/preexisting_results_name-2", ignore_errors=True)
+        output = subprocess.Popen(
+            ["python", "test/example_program.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(
+            ["python", "test/example_program.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(
+            ["python", "test/example_program.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"preexisting_results_name")
+
         self.assertTrue(os.path.exists("results/preexisting_results_name-2/info.txt"))
-        
+
     def testNeedsGitDir(self):
         curr_dir = os.getcwd()
         try:
             os.chdir("..")
             try:
                 import footsteps
+
                 raise Exception("Should have failed")
             except Exception as e:
-                self.assertTrue(str(e) == "code that uses footsteps needs to be run in a git directory to record the git hash assosciated with this experiment")
-        
+                self.assertTrue(
+                    str(e)
+                    == "code that uses footsteps needs to be run in a git directory to record the git hash assosciated with this experiment"
+                )
+
         finally:
             os.chdir(curr_dir)
 
     def testSetNameUsingEnv(self):
+        shutil.rmtree("results/my_results_name/", ignore_errors=True)
         os.environ["FOOTSTEPS_NAME"] = "my_results_name"
-        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"")
+        output = subprocess.Popen(
+            ["python", "test/example_program.py", "horseradish"],
+            stdin=PIPE,
+            stdout=PIPE,
+        ).communicate(b"")
         self.assertTrue(os.path.exists("results/my_results_name/info.txt"))
+        del os.environ["FOOTSTEPS_NAME"]
+
+    def testUncommittedFile(self):
+
+        try:
+            shutil.copy("test/example_program.py", "test/example_uncommitted.py")
+
+            output = subprocess.Popen(
+                ["python", "test/example_uncommitted.py"],
+                stdin=PIPE,
+                stdout=PIPE,
+            ).communicate(b"uncommitted_file")
+
+        finally:
+            pass
+
+    #     os.remove("test/example_uncommitted.py")
